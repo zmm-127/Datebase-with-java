76b8c122a6c096267be54cfef25eb38b7e9047fe
zmm-127 <71617442+zmm-127@users.noreply.github.com>
2020-10-26 12:49:05 +0800

    Create Variable.java

---
+++ v0.2.6/Variable.java
@@ -0,0 +1,19 @@
+package dataBase;
+
+/*
+ * 变量
+ */
+public class Variable {
+
+	private String name; 	//变量名称
+	
+	public Variable(String name)
+	{
+		this.name=name;
+	}
+	public String GetName()
+	{
+		return name;
+	}
+	
+}
