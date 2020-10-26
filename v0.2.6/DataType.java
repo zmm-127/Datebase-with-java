a391342a55f9150bbea8d2f39c507dd6e3c42bb6
zmm-127 <71617442+zmm-127@users.noreply.github.com>
2020-10-26 12:41:31 +0800

    Create DataType.java

---
+++ v0.2.6/DataType.java
@@ -0,0 +1,12 @@
+package staticContent;
+
+public class DataType {
+	static final public int INT=1;		//整数
+	static final public int FLOAT=2;	//小数
+	static final public int STRING=3;	//字符串
+	static final public int BOOL=4;	//布尔值
+//	static final int DATE=5;	//日期
+	static final public int SET=6;		//集合
+	static final public int BLOB=7;	//二进制
+	static final public int NULL=8;	//空值
+}
